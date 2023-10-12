14e6eed5f67a9b80bdeec64c03ec70fcaa8435c1
Vijaya-Paunikar <144651081+Vijaya-Paunikar@users.noreply.github.com>
2023-10-12 17:45:38 +0530

    Add files via upload

---
+++ EmployeeDetail/src/main/java/com/hbm/demo/EmployeeDetail/App.java
@@ -0,0 +1,27 @@
+package com.hbm.demo.EmployeeDetail;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Configuration cfg=new Configuration();
+    	cfg.configure("hibernate.cfg.xml");
+   		SessionFactory factory=cfg.buildSessionFactory();
+   		Session session =factory.openSession();
+   		Transaction tx=session.beginTransaction();
+    	
+        employee emp=new employee();
+        emp.setEmpid(20);
+        emp.setFirstname("Abhishek");
+        emp.setLastname("Hatwar");
+        
+        session.save(emp);
+		tx.commit();
+		session.close();
+    }
+}
