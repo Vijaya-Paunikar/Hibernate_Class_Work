cf6731d48ed83455c369cf49311e94a86d19ea8b
Vijaya-Paunikar <144651081+Vijaya-Paunikar@users.noreply.github.com>
2023-10-12 17:49:45 +0530

    Add files via upload

---
+++ Hiber_Project_Annotations/src/main/java/com/hbm/Student/Hiber_Project_Annotations/App.java
@@ -0,0 +1,13 @@
+package com.hbm.Student.Hiber_Project_Annotations;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
